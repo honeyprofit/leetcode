091ba4e0fd818b5f360a3c3f9e98bab19f1bad21
yhkim <148407209+honeyprofit@users.noreply.github.com>
2024-08-19 21:31:40 +0900

    Create rotateArray.js

---
+++ Array/rotateArray.js
@@ -0,0 +1,21 @@
+var rotate = function(nums, k) {
+    let n = nums.length;
+    k = k % n;
+
+    function reverse(start, end) {
+        while (start < end) {
+            let temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
+    reverse(0, n - 1);
+
+    reverse(0, k - 1);
+
+    reverse(k, n - 1);
+};
