1f5dc706c92702fc27037458b4bbf9b230321ae2
yhkim <148407209+honeyprofit@users.noreply.github.com>
2024-09-17 17:49:09 +0900

    Create mostWater.js

---
+++ TwoPointers/mostWater.js
@@ -0,0 +1,22 @@
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
+
+    while ( left < right) {
+        let width = right - left;
+        let currentHeight = Math.min(height[left], height[right]);
+        let currentArea = width * currentHeight;
+
+        maxArea = Math.max(maxArea, currentArea);
+
+        if(height[left] < height[right]){
+            left++;
+        } else { 
+            right--;
+        }
+    }
+
+    return maxArea;
+
+};
