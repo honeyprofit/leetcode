1ffc81310250eea9566e0cb0488c49d07e2559ad
yhkim <148407209+honeyprofit@users.noreply.github.com>
2024-09-29 20:30:16 +0900

    Create AddElement2.jsx

---
+++ React/AddElement2.jsx
@@ -0,0 +1,10 @@
+const [colors, setColors] = useState(['red', 'green']);
+
+const addColorAtIndex = (colorToAdd, index) => {
+  const updatedColors = [
+    ...colors.slice(0, index),
+    colorToAdd,
+    ...colors.slice(index),
+  ];
+  setColors(updatedColors);
+};
