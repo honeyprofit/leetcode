bb08a677126479e66f6e3e9da87e218888716c71
yhkim <148407209+honeyprofit@users.noreply.github.com>
2024-10-06 17:40:36 +0900

    Create ChangePrpoerty.jsx

---
+++ React/ChangePrpoerty.jsx
@@ -0,0 +1,13 @@
+const [fruit, setFruit] = useState({
+  color: 'red',
+  name: 'apple',
+});
+
+const changeColor = (newColor) => {
+  const updatedFruit = {
+    ...fruit,
+    color: newColor,
+  };
+
+  setFruit(updatedFruit);
+};
