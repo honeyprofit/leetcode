5e7d743569442d4dc7c02fe67ccf157595b974bc
yhkim <148407209+honeyprofit@users.noreply.github.com>
2024-11-12 23:56:44 +0900

    Update sort.js

--- sort.js
+++ sort.js
@@ -3,10 +3,14 @@ const data = [    { name: "Orange", color: 7, score: 5 },
     { name: "Banana", color: 9, score: 1 },
     { name: "Lime", color: 1, score: 4 },];
 function getSortValue(vegetable){return vegetable.color;}
+const sortOrder = 'asc'; /*desc*/
 data.sort((a,b)=>{
     const valueA = getSortValue(a);
     const valueB = getSortValue(b);
+    const reverseOrder = sortOrder === 'asc'?1:-1;
     if(typeof valueA === 'string'){
-        return valueA.localeCompare(valueB);
+        return valueA.localeCompare(valueB)*reverseOrder;
     } else{
-    return valueA - valueB;}})
+    return (valueA - valueB)*reverseOrder;
+    }
+})
