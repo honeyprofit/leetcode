5550a37f6750aa3c6a3e09e2ee18699b555d4228
yhkim <148407209+honeyprofit@users.noreply.github.com>
2024-12-15 19:09:32 +0900

    Create 190_Timers_setTimeoutandsetInterval.js

---
+++ NumberandDates/190_Timers_setTimeoutandsetInterval.js
@@ -0,0 +1,11 @@
+//190_Timers: setTimeout and setInterval
+
+const ingredients = ['mushroom', 'tomato'];
+const pizzaTimer = setTimeout(
+  (ing1, ing2) => console.log(`Here is your pizza with ${ing1} and ${ing2}`),
+  3000,
+  ...ingredients
+);
+console.log('Waiting...');
+
+if (ingredients.includes('tomato')) clearTimeout(pizzaTimer);
