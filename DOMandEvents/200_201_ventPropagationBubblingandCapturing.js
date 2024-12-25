191c0758de4093b68f7498ec6207658cb8fb037b
yhkim <148407209+honeyprofit@users.noreply.github.com>
2024-12-25 21:40:14 +0900

    Create 200_201_ventPropagationBubblingandCapturing.js

---
+++ DOMandEvents/200_201_ventPropagationBubblingandCapturing.js
@@ -0,0 +1,26 @@
+//200. Event Propagation: Bubbling and Capturing
+//201. Event Propagation in Practice
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Link', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  //Stop propagation
+  e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('container', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('nav', e.target, e.currentTarget);
+});
