fd204458a1d7c83d1de9e7442afefa210c96f190
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-01-01 20:14:03 +0900

    Create 204_BuildingaTabbedComponent.js

---
+++ DOMandEvents/204_BuildingaTabbedComponent.js
@@ -0,0 +1,23 @@
+//204. Building a Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  //Guard clause
+  if (!clicked) return;
+
+  //Activate tab
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+
+  //Activate content area
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
