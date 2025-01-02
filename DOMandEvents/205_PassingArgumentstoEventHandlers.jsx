136665e45534a5e2c060e9833a8142253026b711
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-01-02 22:42:45 +0900

    Create 205_PassingArgumentstoEventHandlers.jsx

---
+++ DOMandEvents/205_PassingArgumentstoEventHandlers.jsx
@@ -0,0 +1,18 @@
+//205. Passing Arguments to Event Handlers
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
