7a8748f031aae6869292655aaa61def8027ac8d9
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-01-07 00:27:25 +0900

    Update 207_ABetterWayTheIntersectionObserverAPI.js

--- DOMandEvents/207_ABetterWayTheIntersectionObserverAPI.js
+++ DOMandEvents/207_ABetterWayTheIntersectionObserverAPI.js
@@ -1,4 +1,32 @@
 //207. A Better Way: The Intersection Observer API
+const obsCallback = function (entries, observer) {
+  entries.forEach(entry => {
+    console.log(entry);
+  });
+};
 
-const observer = new IntersectionObserver();
+const obsOptions = {
+  root: null,
+  threshold: [0, 0.2],
+};
+
+const observer = new IntersectionObserver(obsCallback, obsOptions);
 observer.observe(section1);
+*/
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
