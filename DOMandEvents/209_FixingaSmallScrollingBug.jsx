43dddf418e6ddb9a39b926bc9610456a7cae21df
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-01-08 20:19:13 +0900

    Create 209_FixingaSmallScrollingBug.jsx

---
+++ DOMandEvents/209_FixingaSmallScrollingBug.jsx
@@ -0,0 +1,23 @@
+//209. Fixing a Small Scrolling Bug
+
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  console.log(entries);
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
