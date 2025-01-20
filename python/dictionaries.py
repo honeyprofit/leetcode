ee4db5288595e4f301e19155a963fca200ee9ffb
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-01-20 23:42:33 +0900

    Update dictionaries.py

--- python/dictionaries.py
+++ python/dictionaries.py
@@ -3,24 +3,23 @@ programming_dictionary = {
     "Function": "A piece of code that you can easily call over and over again.",
 }
 
-# Retrieving a value from a dictionary
-print(programming_dictionary["Function"])
+# print(programming_dictionary["Bug"])
+print(programming_dictionary)
 
-# Adding more items to a dictionary
-programming_dictionary["Loop"] = "The action of doing something over and over again."
+programming_dictionary["Loop"] = "A action of doing something over and over again."
+print(programming_dictionary)
 
-# Creating an empty dictionary
+
+# clear dictionary
 empty_dictionary = {}
-
-# Wipe an existing dictionary
-# programming_dictionary = {}
-# print(programming_dictionary)
+programming_dictionary = empty_dictionary
+print(programming_dictionary)
 
 # Edit an item in a dictionary
-programming_dictionary["Bug"] = "A moth in your computer."
-# print(programming_dictionary)
+programming_dictionary["Bug"]= "A moth in your computer"
+print(programming_dictionary)
 
-# Loop through a dictionary
+#Loop through a dictionary
 for key in programming_dictionary:
     print(key)
     print(programming_dictionary[key])
