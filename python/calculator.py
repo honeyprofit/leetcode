87f152e7c30867dd65b8e357c858c4c90e0dbbdd
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-02-01 22:19:05 +0900

    Create calculator.py

---
+++ python/calculator.py
@@ -0,0 +1,65 @@
+logo = r"""
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+
+from random import choice
+import art
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
+
+# print(operations["*"](4,8))
+
+# print(logo)
+def calculator():
+    should_accumulate = True
+    print(art.logo)
+    num1 = float(input("What is the first number?"))
+
+    while(should_accumulate):
+        for symbol in operations:
+            print(symbol)
+        operation = input ("Pick an operation: ")
+        num2 = float(input("What is the next number?"))
+        answer = operations[operation](num1, num2)
+        print(f"{num1} {operation} {num2} = {answer}")
+
+        choice = input(f"Type 'y' to continue calcurating with {answer}, or type 'n' to start a new: ")
+        if(choice) == "y":
+            num1 = answer
+        else:
+            should_accumulate = False
+            print("\n"*20)
+            calculator()
+
+calculator()
