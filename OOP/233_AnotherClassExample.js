d70dbd3a43cd8a3d4eb7a94ace45abf3b0d0814d
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-02-24 21:52:24 +0900

    Create 233_AnotherClassExample.js

---
+++ OOP/233_AnotherClassExample.js
@@ -0,0 +1,45 @@
+////////////////
+//233. Another Class Example
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.local = navigator.language;
+
+    console.log(`Thanks for opening the account, ${owner}`);
+  }
+
+  //Public interface
+  deposit(val) {
+    this.movements.push(val);
+  }
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+console.log(acc1);
+
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
+console.log(acc1.pin);
