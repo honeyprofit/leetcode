030304b7665c6727ef6f3149f14e83868074a5e2
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-03-27 22:03:34 +0900

    Create 438_TheAnyandUnknownTypes.tsx

---
+++ TypeScript/438_TheAnyandUnknownTypes.tsx
@@ -0,0 +1,22 @@
+////////////////
+//438. The "Any" and "Unknown" Types
+
+interface Book {
+    title: string;
+}
+
+async function fetchBook(): Promise<Book> {
+    const res = await fetch('/book');
+    const data: unknown = await res.json();
+
+    if(data && typeof data === 'object' && 'title' in data && typeof data.title === 'string') {
+        return data as Book;
+    }
+
+    throw new Error('Expected to get a book, but didnt')
+    // return data as Book;
+}
+
+async function run() {
+    const book = await fetchBook();
+}
