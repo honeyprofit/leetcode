63139d7da7ec22ca105bded1c37e5a77c9dfce20
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-05-04 22:00:39 +0900

    Update 442_MultipleGenericsTypes.tsx

--- TypeScript/442_MultipleGenericsTypes.tsx
+++ TypeScript/442_MultipleGenericsTypes.tsx
@@ -7,5 +7,15 @@ function toRecord <TypeOfID> (id: TypeOfID, value:string){
     };
 }
 
-const result5 = toRecord<number>(123, 'my@email.com')
-const return6 = toRecord<string>('ea01', 'your@email.com')
+const result3 = toRecord<number>(123, 'my@email.com')
+const return4 = toRecord<string>('ea01', 'your@email.com')
+
+function toRecord2 <TypeOfID, TypeOfValue> (id: TypeOfID, value:TypeOfValue){
+    return{
+        id,
+        value
+    };
+}
+
+const result5 = toRecord<number, string>(123, 'my@email.com')
+const return6 = toRecord<string, number[]>('ea01', 'your@email.com')
