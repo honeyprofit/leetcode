5ff3465ef818b43642446339ec3142e1ebc98d40
yhkim <148407209+honeyprofit@users.noreply.github.com>
2025-06-11 13:54:31 +0900

    Update 22_form_post.php

--- PHP/web2-php/22_form_post.php
+++ PHP/web2-php/22_form_post.php
@@ -1,3 +1,19 @@
+<!doctype html>
+<!-- <html>
+  <body>
+    <form action="form.php" method="post"> //form.php?title=PHP&description=PHP+is...
+      <p><input type="text" name="title" placeholder="Title"></p>
+      <p><textarea name="description"></textarea></p>
+      <p><input type="submit"></p>
+    </form>
+  </body>
+</html> -->
+
+
+
 <?php
+echo "<p>title :".&_GET['title']."</P>";
+echo "<p>description :".&_GET['description']."</P>";
+
 file_put_contents('data/'.$_POST['title'], $_POST['description']);
 ?>
